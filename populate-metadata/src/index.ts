4dba009af3b3f08356dba50d7d0612e6b5180c68
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-10-22 15:38:07 -0400

    DOP-5071 try passing in env vars in initialization

--- populate-metadata/src/index.ts
+++ populate-metadata/src/index.ts
@@ -6,8 +6,12 @@ const extension = new Extension({
   isEnabled: envVarToBool(process.env.POPULATE_METADATA_ENABLED),
 });
 
-extension.addBuildEventHandler('onPreBuild', async ({ netlifyConfig }) => {
-  await updateConfig(netlifyConfig);
-});
+extension.addBuildEventHandler(
+  'onPreBuild',
+  async ({ netlifyConfig, envVars }) => {
+    console.log('ENV VARS in index', envVars);
+    await updateConfig(netlifyConfig);
+  },
+);
 
 export { extension };
--- populate-metadata/src/updateConfig.ts
+++ populate-metadata/src/updateConfig.ts
@@ -2,6 +2,7 @@ import { getProperties } from './getProperties';
 import type { DocsConfig } from './types';
 
 export const updateConfig = async (config: DocsConfig) => {
+  console.log('config in updateConfig:', config);
   const branchName =
     process.env.BRANCH_NAME ?? config.build?.environment.BRANCH;
   const repoName = process.env.REPO_NAME ?? config.build?.environment.SITE_NAME;
@@ -21,9 +22,9 @@ export const updateConfig = async (config: DocsConfig) => {
   // Check if this was an engineering build or writer's build; writer's builds by default are all builds not built on the "mongodb-snooty" site
   // Environment is either dotcomprd or prd if it is a writer build
   if (config.build.environment.SITE_NAME !== 'mongodb-snooty') {
-    process.env.ENV = isWebhookDeploy ? 'dotcomprd' : 'prd';
+    config.build.environment.ENV = isWebhookDeploy ? 'dotcomprd' : 'prd';
   } else {
-    process.env.ENV = isWebhookDeploy ? 'dotcomstg' : 'stg';
+    config.build.environment.ENV = isWebhookDeploy ? 'dotcomstg' : 'stg';
   }
 
   const { repo, docsetEntry } = await getProperties({
