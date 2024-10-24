35f44e10b6778865c8a16b1c17530cec890b441b
biancalaube <bianca.laube@gmail.com>
2024-10-24 16:29:11 -0400

    updates

--- redirects-and-publish/src/getProperties.ts
+++ redirects-and-publish/src/getProperties.ts
@@ -1,71 +1,71 @@
 import type {
-	DatabaseDocument,
-	DocsetsDocument,
-	ReposBranchesDocument,
-} from "./types";
-import { getCollection, getSnootyDb } from "./searchConnector";
-import type { Collection } from "mongodb";
+  DatabaseDocument,
+  DocsetsDocument,
+  ReposBranchesDocument,
+} from './types';
+import { getCollection, getSnootyDb } from './searchConnector';
+import type { Collection } from 'mongodb';
 
 export const getProperties = async (repoName: string) => {
-	//connect to database and get reposBranches, docsets collections
-	console.log("connectiong to mongodb...");
-	const dbSession = await getSnootyDb();
-	const reposBranches = getCollection(dbSession, "repos_branches");
-	const docsets = getCollection(dbSession, "docsets");
+  //connect to database and get reposBranches, docsets collections
+  console.log('connectiong to mongodb...');
+  const dbSession = await getSnootyDb();
+  const reposBranches = getCollection(dbSession, 'repos_branches');
+  const docsets = getCollection(dbSession, 'docsets');
 
-	console.log("querying repobranches...");
-	const repo: ReposBranchesDocument = await getRepoEntry({
-		repoName: repoName,
-		reposBranches,
-	});
+  console.log('querying repobranches...');
+  const repo: ReposBranchesDocument = await getRepoEntry({
+    repoName: repoName,
+    reposBranches,
+  });
 
-	console.log("querying docsets...");
-	const { project } = repo;
-	const docsetEntry: DocsetsDocument = await getDocsetEntry(docsets, project);
+  console.log('querying docsets...');
+  const { project } = repo;
+  const docsetEntry: DocsetsDocument = await getDocsetEntry(docsets, project);
 
-	return docsetEntry;
+  return docsetEntry;
 };
 
 export const getDocsetEntry = async (
-	docsets: Collection<DatabaseDocument>,
-	project: string,
+  docsets: Collection<DatabaseDocument>,
+  project: string,
 ) => {
-	const docsetsQuery = { project: { $eq: project } };
-	const docset = await docsets.findOne<DocsetsDocument>(docsetsQuery);
-	if (!docset) {
-		throw new Error("Error while getting docsets entry in Atlas");
-	}
-	return docset;
+  const docsetsQuery = { project: { $eq: project } };
+  const docset = await docsets.findOne<DocsetsDocument>(docsetsQuery);
+  if (!docset) {
+    throw new Error('Error while getting docsets entry in Atlas');
+  }
+  return docset;
 };
 
 export const getRepoEntry = async ({
-	repoName,
-	reposBranches,
+  repoName,
+  reposBranches,
 }: {
-	repoName: string;
-	reposBranches: Collection<DatabaseDocument>;
+  repoName: string;
+  reposBranches: Collection<DatabaseDocument>;
 }) => {
-	const query = {
-		repoName: repoName,
-	};
+  const query = {
+    repoName: repoName,
+  };
 
-	const repo = await reposBranches.findOne<ReposBranchesDocument>(query, {
-		projection: {
-			_id: 0,
-			project: 1,
-			search: 1,
-			branches: 1,
-			prodDeployable: 1,
-			internalOnly: 1,
-		},
-	});
-	if (!repo) {
-		throw new Error(
-			`Could not get reposBranches entry for repo ${repoName}, ${repo}, ${JSON.stringify(
-				query,
-			)}`,
-		);
-	}
+  const repo = await reposBranches.findOne<ReposBranchesDocument>(query, {
+    projection: {
+      _id: 0,
+      project: 1,
+      search: 1,
+      branches: 1,
+      prodDeployable: 1,
+      internalOnly: 1,
+    },
+  });
+  if (!repo) {
+    throw new Error(
+      `Could not get reposBranches entry for repo ${repoName}, ${repo}, ${JSON.stringify(
+        query,
+      )}`,
+    );
+  }
 
-	return repo;
+  return repo;
 };
--- redirects-and-publish/src/index.ts
+++ redirects-and-publish/src/index.ts
@@ -6,76 +6,79 @@ const extension = new NetlifyExtension();
 
 const MUT_VERSION = '0.11.4';
 
-extension.addBuildEventHandler('onSuccess', async ({ utils: { status, git, run }, netlifyConfig }) => {
-  if (!process.env.MUT_COMMANDS_ENABLED) return;
+extension.addBuildEventHandler(
+  'onSuccess',
+  async ({ utils: { status, git, run }, netlifyConfig }) => {
+    if (!process.env.MUT_COMMANDS_ENABLED) return;
 
-  // running mut-redirects -------------------------------------------------------
-  const redirectErrs = '';
-  console.log('Downloading Mut...');
-  await run('curl', [
-    '-L',
-    '-o',
-    'mut.zip',
-    `https://github.com/mongodb/mut/releases/download/v${MUT_VERSION}/mut-v${MUT_VERSION}-linux_x86_64.zip`,
-  ]);
-  await run.command('unzip -d . -qq mut.zip');
-  try {
-    console.log('Running mut-redirects...');
-    await run.command(
-      `${process.cwd()}/mut/mut-redirects config/redirects -o snooty/public/.htaccess`,
-    );
-  } catch (e) {
-    console.log(`Error occurred while running mut-redirects: ${e}`);
-  }
+    // running mut-redirects -------------------------------------------------------
+    const redirectErrs = '';
+    console.log('Downloading Mut...');
+    await run('curl', [
+      '-L',
+      '-o',
+      'mut.zip',
+      `https://github.com/mongodb/mut/releases/download/v${MUT_VERSION}/mut-v${MUT_VERSION}-linux_x86_64.zip`,
+    ]);
+    await run.command('unzip -d . -qq mut.zip');
+    try {
+      console.log('Running mut-redirects...');
+      await run.command(
+        `${process.cwd()}/mut/mut-redirects config/redirects -o snooty/public/.htaccess`,
+      );
+    } catch (e) {
+      console.log(`Error occurred while running mut-redirects: ${e}`);
+    }
 
-  // running mut-publish ----------------------------------------------------------
-  //TODO: change these teamwide env vars in Netlify UI when ready to move to prod
-  process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_S3_SECRET_ACCESS_KEY;
-  process.env.AWS_ACCESS_KEY_ID = process.env.AWS_S3_ACCESS_KEY_ID;
+    // running mut-publish ----------------------------------------------------------
+    //TODO: change these teamwide env vars in Netlify UI when ready to move to prod
+    process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_S3_SECRET_ACCESS_KEY;
+    process.env.AWS_ACCESS_KEY_ID = process.env.AWS_S3_ACCESS_KEY_ID;
 
-  console.log("start of the mut-publish plugin -----------");
+    console.log('start of the mut-publish plugin -----------');
 
-  const repoName = process.env.REPO_NAME ?? netlifyConfig.build.environment.SITE_NAME;
-  if (!repoName) {
-    throw new Error(
-      "No repo name supplied as environment variable, manifest cannot be uploaded to Atlas Search.Documents collection ",
-    );
-  }
-  console.log("repo name is:", repoName);
+    const repoName =
+      process.env.REPO_NAME ?? netlifyConfig.build.environment.SITE_NAME;
+    if (!repoName) {
+      throw new Error(
+        'No repo name supplied as environment variable, manifest cannot be uploaded to Atlas Search.Documents collection ',
+      );
+    }
+    console.log('repo name is:', repoName);
 
-  // connect to mongodb and pool.docsets to get bucket
-  const docsetEntry = await getProperties(repoName);
-  console.log("Succesfully got docsets entry:", docsetEntry);
+    // connect to mongodb and pool.docsets to get bucket
+    const docsetEntry = await getProperties(repoName);
+    console.log('Succesfully got docsets entry:', docsetEntry);
 
-  /*Usage: mut-publish <source> <bucket> --prefix=prefix
+    /*Usage: mut-publish <source> <bucket> --prefix=prefix
                     (--stage|--deploy)
                     [--all-subdirectories]
                     [--redirects=htaccess]
                     [--deployed-url-prefix=prefix]
                     [--redirect-prefix=prefix]...
                     [--dry-run] [--verbose] [--json] */
-  try {
-    console.log("Running mut-publish...");
-    // TODO: do I need to log this command below
-    // TODO: "do I need to change the prefix to be: docsEntry.prefix.dotcomstg, or do we want to add /netlify"
-    // also, in production do we leave it as dotcomstg or what bucket should it be changed to?
-    await run(
-      `${process.cwd()}/mut/mut-publish`,
-      [
-        "snooty/public",
-        docsetEntry.bucket.dotcomstg,
-        "--prefix=/netlify/docs-qa",
-        "--deploy",
-        `--deployed-url-prefix=${docsetEntry.url.dotcomstg}`,
-        "--json",
-        "--all-subdirectories",
-      ],
-      { input: "y" },
-    );
-  } catch (e) {
-    console.log(`Error occurred while running mut-publish: ${e}`);
-  }
-
-});
+    try {
+      console.log('Running mut-publish...');
+      // TODO: do I need to log this command below
+      // TODO: "do I need to change the prefix to be: docsEntry.prefix.dotcomstg, or do we want to add /netlify"
+      // also, in production do we leave it as dotcomstg or what bucket should it be changed to?
+      await run(
+        `${process.cwd()}/mut/mut-publish`,
+        [
+          'snooty/public',
+          docsetEntry.bucket.dotcomstg,
+          '--prefix=/netlify/docs-qa',
+          '--deploy',
+          `--deployed-url-prefix=${docsetEntry.url.dotcomstg}`,
+          '--json',
+          '--all-subdirectories',
+        ],
+        { input: 'y' },
+      );
+    } catch (e) {
+      console.log(`Error occurred while running mut-publish: ${e}`);
+    }
+  },
+);
 
 export { extension };
