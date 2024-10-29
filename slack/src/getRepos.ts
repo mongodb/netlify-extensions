fbf99f267c3b7419a2807aa31c5ee5ec73d22a77
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-10-29 13:39:59 -0400

    feat/slack linting

--- slack/src/getRepos.ts
+++ slack/src/getRepos.ts
@@ -1,12 +1,12 @@
-import * as mongodb from "mongodb";
-import type { ReposBranchesDocument } from "../../search-manifest/src/types.js";
-import { buildRepoGroups } from "./build-modal.js";
+import type * as mongodb from 'mongodb';
+import type { ReposBranchesDocument } from '../../search-manifest/src/types.js';
+import { buildRepoGroups } from './build-modal.js';
 
 export async function getDeployableRepos(
-  reposBranchesColl: mongodb.Collection<ReposBranchesDocument>
+	reposBranchesColl: mongodb.Collection<ReposBranchesDocument>,
 ) {
-  const query = { prodDeployable: true, internalOnly: false };
-  const options = { projection: { repoName: 1, branches: 1 } };
-  const cursor = reposBranchesColl.find(query, options);
-  return await buildRepoGroups(cursor);
+	const query = { prodDeployable: true, internalOnly: false };
+	const options = { projection: { repoName: 1, branches: 1 } };
+	const cursor = reposBranchesColl.find(query, options);
+	return await buildRepoGroups(cursor);
 }
