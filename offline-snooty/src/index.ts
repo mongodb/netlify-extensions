7699674af2a09f21a18831835937858aebc3f6f8
Seung Park <seung.park@mongodb.com>
2024-11-01 09:49:23 -0400

    make copy of offline snooty and build

--- offline-snooty/src/index.ts
+++ offline-snooty/src/index.ts
@@ -1,15 +1,28 @@
 // Documentation: https://sdk.netlify.com
 import { NetlifyExtension } from "@netlify/sdk";
+import { createSnootyCopy } from "./createSnootyCopy";
 
 const extension = new NetlifyExtension();
+const NEW_SNOOTY_PATH = `${process.cwd()}/snooty-offline`;
 
-extension.addBuildEventHandler("onPreBuild", () => {
+// run this extension after the build and deploy are successful
+extension.addBuildEventHandler("onSuccess", async ({ utils: { run } }) => {
   // If the build event handler is not enabled, return early
   if (!process.env["OFFLINE_SNOOTY_ENABLED"]) {
     return;
   }
-  console.log("Hello there.");
-});
-  
-export { extension };
+  console.log("snooty offline onSuccess.");
+  try {
+    await createSnootyCopy(run, NEW_SNOOTY_PATH);
+    // await processGatsbyOutput
+  }
+  // convertSnootyToHtml('/public')
 
+  // new module
+  // convertSnootyToHtml:
+
+  // if (!(html or folder)) { delete file }
+  //
+});
+
+export { extension };
