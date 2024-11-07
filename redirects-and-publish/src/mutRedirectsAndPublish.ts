3dd5b27002068e2d27501692dea942c61ccaa74f
biancalaube <bianca.laube@gmail.com>
2024-11-07 13:59:57 -0500

    formatting

--- redirects-and-publish/src/mutRedirectsAndPublish.ts
+++ redirects-and-publish/src/mutRedirectsAndPublish.ts
@@ -1,138 +1,148 @@
-import type { ConfigEnvironmentVariables } from "./types";
+import type { ConfigEnvironmentVariables } from './types';
 import type { NetlifyPluginUtils } from '@netlify/build';
 
 const MUT_VERSION = '0.11.4';
-const MANIFEST_PATH = `${process.cwd()}/bundle.zip`
+const MANIFEST_PATH = `${process.cwd()}/bundle.zip`;
 
 export const mutRedirectsAndPublish = async (
-    configEnvironment: ConfigEnvironmentVariables,
-    run: NetlifyPluginUtils['run'],
+  configEnvironment: ConfigEnvironmentVariables,
+  run: NetlifyPluginUtils['run'],
 ): Promise<void> => {
+  // we want to copy the snooty folder and run `npm run build` instead of `npm run build:no-prefix` as it does in the build.sh --------------------------
+  // we do this so when we run mut-publish we are able to uplaod the correct files with the correct paths
+  await run.command('rm -f -r running-mut');
+  await run.command('mkdir -p running-mut');
 
-    // we want to copy the snooty folder and run `npm run build` instead of `npm run build:no-prefix` as it does in the build.sh --------------------------
-    // we do this so when we run mut-publish we are able to uplaod the correct files with the correct paths
-    await run.command('rm -f -r running-mut');
-    await run.command('mkdir -p running-mut');
-
-    if (configEnvironment?.SITE_NAME === 'mongodb-snooty') { // since mongodb-snooty is not a content repo the file structure is different and needs to be treated as such
-      await run.command('rm -f -r running-mut/snooty');
-      await run.command('mkdir -p running-mut/snooty');
-      await run.command('cp -r AWSCLIV2.pkg build.sh bundle.zip CHANGELOG.md code-of-conduct.md component-factory-transformer docs-landing gatsby-browser.js gatsby-config.js gatsby-ssr.js jest.config.js jest-preprocess.js Makefile __mocks__ netlify.toml node_modules package.json package-lock.json plugins public README.md scripts snooty-parser snooty-parser.zip src static stubs tests running-mut/snooty/');
-    } else {
-      await run.command('cp -r snooty running-mut');
-      await run.command('ls running-mut/snooty');
-    }
-
+  if (configEnvironment?.SITE_NAME === 'mongodb-snooty') {
+    // since mongodb-snooty is not a content repo the file structure is different and needs to be treated as such
+    await run.command('rm -f -r running-mut/snooty');
+    await run.command('mkdir -p running-mut/snooty');
+    await run.command(
+      'cp -r AWSCLIV2.pkg build.sh bundle.zip CHANGELOG.md code-of-conduct.md component-factory-transformer docs-landing gatsby-browser.js gatsby-config.js gatsby-ssr.js jest.config.js jest-preprocess.js Makefile __mocks__ netlify.toml node_modules package.json package-lock.json plugins public README.md scripts snooty-parser snooty-parser.zip src static stubs tests running-mut/snooty/',
+    );
+  } else {
+    await run.command('cp -r snooty running-mut');
     await run.command('ls running-mut/snooty');
-    process.chdir(`${process.cwd()}/running-mut/snooty`);
-    process.env.GATSBY_MANIFEST_PATH = MANIFEST_PATH;
-    process.env.PATH_PREFIX = '/docs-qa';
-    process.env.GATSBY_PARSER_USER='buildbot';
-    await run.command('npm run clean');
-    await run.command('npm run build');
+  }
 
-    // running mut-redirects -------------------------------------------------------
-    console.log('Downloading Mut...',configEnvironment?.SITE_NAME );
+  await run.command('ls running-mut/snooty');
+  process.chdir(`${process.cwd()}/running-mut/snooty`);
+  process.env.GATSBY_MANIFEST_PATH = MANIFEST_PATH;
+  process.env.PATH_PREFIX = '/docs-qa';
+  process.env.GATSBY_PARSER_USER = 'buildbot';
+  await run.command('npm run clean');
+  await run.command('npm run build');
 
-    await run('curl', [
-      '-L',
-      '-o',
-      'mut.zip',
-      `https://github.com/mongodb/mut/releases/download/v${MUT_VERSION}/mut-v${MUT_VERSION}-linux_x86_64.zip`,
-    ]);
+  // running mut-redirects -------------------------------------------------------
+  console.log('Downloading Mut...', configEnvironment?.SITE_NAME);
 
-    await run.command('unzip -d . -qq mut.zip');
+  await run('curl', [
+    '-L',
+    '-o',
+    'mut.zip',
+    `https://github.com/mongodb/mut/releases/download/v${MUT_VERSION}/mut-v${MUT_VERSION}-linux_x86_64.zip`,
+  ]);
 
-    try {
-      console.log('Running mut-redirects...');
-      
-      if (configEnvironment?.SITE_NAME === "mongodb-snooty") {
-        // so mongodb-snooty can launch with docs-landing
-        await run.command(
-          `${process.cwd()}/mut/mut-redirects docs-landing/config/redirects -o public/.htaccess`,
-        );
-      } else {
-        await run.command(
-          `${process.cwd()}/mut/mut-redirects ../../config/redirects -o public/.htaccess`,
-        );
-     }
-    } catch (e) {
-      console.log(`Error occurred while running mut-redirects: ${e}`);
-    }
+  await run.command('unzip -d . -qq mut.zip');
 
-    // running mut-publish ----------------------------------------------------------
-    //TODO: change these teamwide env vars in Netlify UI when ready to move to prod
-    process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_S3_SECRET_ACCESS_KEY;
-    process.env.AWS_ACCESS_KEY_ID = process.env.AWS_S3_ACCESS_KEY_ID;
+  try {
+    console.log('Running mut-redirects...');
 
-    if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-      throw new Error('Credentials not found');
-    }
-
-    console.log('Start of the mut-publish plugin -----------');
-
-    const repoName =
-      process.env.REPO_NAME ?? configEnvironment?.SITE_NAME;
-
-    if (!repoName) {
-      throw new Error(
-        'No repo name supplied as environment variable, manifest cannot be uploaded to Atlas Search.Documents collection ',
+    if (configEnvironment?.SITE_NAME === 'mongodb-snooty') {
+      // so mongodb-snooty can launch with docs-landing
+      await run.command(
+        `${process.cwd()}/mut/mut-redirects docs-landing/config/redirects -o public/.htaccess`,
+      );
+    } else {
+      await run.command(
+        `${process.cwd()}/mut/mut-redirects ../../config/redirects -o public/.htaccess`,
       );
     }
-    console.log('Repo name is:', repoName);
+  } catch (e) {
+    console.log(`Error occurred while running mut-redirects: ${e}`);
+  }
 
-    // connect to mongodb and pool.docsets to get bucket
-    const docsetEntry = configEnvironment?.DOCSET_ENTRY;
-    console.log('Succesfully got docsets entry:', docsetEntry);
+  // running mut-publish ----------------------------------------------------------
+  //TODO: change these teamwide env vars in Netlify UI when ready to move to prod
+  process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_S3_SECRET_ACCESS_KEY;
+  process.env.AWS_ACCESS_KEY_ID = process.env.AWS_S3_ACCESS_KEY_ID;
 
-    /*Usage: mut-publish <source> <bucket> --prefix=prefix
+  if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
+    throw new Error('Credentials not found');
+  }
+
+  console.log('Start of the mut-publish plugin -----------');
+
+  const repoName = process.env.REPO_NAME ?? configEnvironment?.SITE_NAME;
+
+  if (!repoName) {
+    throw new Error(
+      'No repo name supplied as environment variable, manifest cannot be uploaded to Atlas Search.Documents collection ',
+    );
+  }
+  console.log('Repo name is:', repoName);
+
+  // connect to mongodb and pool.docsets to get bucket
+  const docsetEntry = configEnvironment?.DOCSET_ENTRY;
+  console.log('Succesfully got docsets entry:', docsetEntry);
+
+  /*Usage: mut-publish <source> <bucket> --prefix=prefix
                     (--stage|--deploy)
                     [--all-subdirectories]
                     [--redirects=htaccess]
                     [--deployed-url-prefix=prefix]
                     [--redirect-prefix=prefix]...
                     [--dry-run] [--verbose] [--json] */
-    try {
-      console.log('Running mut-publish...');
-      if (!docsetEntry?.bucket || !docsetEntry?.prefix || !docsetEntry?.url) {
-        throw new Error;
-      }
-
-      console.log('In the bucket of', docsetEntry?.bucket, docsetEntry?.bucket?.dotcomstg); // subbed in docs-mongodb-org-dotcomstg
-      console.log('With a prefix of', docsetEntry?.prefix, `--prefix=${docsetEntry?.prefix?.dotcomstg}`); // subbed in /netlify/docs-qa
-      console.log('And a URL of: ',  docsetEntry?.url); // https://mongodbcom-cdn.website.staging.corp.mongodb.com/
-      
-      // TODO: do I need to log this command below ?
-      if (docsetEntry?.bucket?.dotcomstg !== undefined) {
-        await run(
-          `${process.cwd()}/mut/mut-publish`,
-          [
-            'public',
-            `${docsetEntry?.bucket?.dotcomstg}`,
-            `--prefix=/${docsetEntry?.prefix?.dotcomstg}`,
-            '--deploy',
-            `--deployed-url-prefix=s${docsetEntry?.url?.dotcomstg}`,
-            '--json',
-            '--all-subdirectories',
-          ],
-          { input: 'y' },
-        );
-      } else if (docsetEntry?.bucket?.dotcomprd !== undefined) { // we will want to do this in the future but not in this PR
-        // await run(
-        //   `${process.cwd()}/mut/mut-publish`,
-        //   [
-        //     'public',
-        //     `${docsetEntry?.bucket?.dotcomprd}`,
-        //     `--prefix=/${docsetEntry?.prefix?.dotcomprd}`,
-        //     '--deploy',
-        //     `--deployed-url-prefix=s${docsetEntry?.url?.dotcomprd}`,
-        //     '--json',
-        //     '--all-subdirectories',
-        //   ],
-        //   { input: 'y' },
-        // );
-      }
-    } catch (e) {
-      console.log(`Error occurred while running mut-publish: ${e}`);
+  try {
+    console.log('Running mut-publish...');
+    if (!docsetEntry?.bucket || !docsetEntry?.prefix || !docsetEntry?.url) {
+      throw new Error();
     }
-}
+
+    console.log(
+      'In the bucket of',
+      docsetEntry?.bucket,
+      docsetEntry?.bucket?.dotcomstg,
+    ); // subbed in docs-mongodb-org-dotcomstg
+    console.log(
+      'With a prefix of',
+      docsetEntry?.prefix,
+      `--prefix=${docsetEntry?.prefix?.dotcomstg}`,
+    ); // subbed in /netlify/docs-qa
+    console.log('And a URL of: ', docsetEntry?.url); // https://mongodbcom-cdn.website.staging.corp.mongodb.com/
+
+    // TODO: do I need to log this command below ?
+    if (docsetEntry?.bucket?.dotcomstg !== undefined) {
+      await run(
+        `${process.cwd()}/mut/mut-publish`,
+        [
+          'public',
+          `${docsetEntry?.bucket?.dotcomstg}`,
+          `--prefix=/${docsetEntry?.prefix?.dotcomstg}`,
+          '--deploy',
+          `--deployed-url-prefix=s${docsetEntry?.url?.dotcomstg}`,
+          '--json',
+          '--all-subdirectories',
+        ],
+        { input: 'y' },
+      );
+    } else if (docsetEntry?.bucket?.dotcomprd !== undefined) {
+      // we will want to do this in the future but not in this PR
+      // await run(
+      //   `${process.cwd()}/mut/mut-publish`,
+      //   [
+      //     'public',
+      //     `${docsetEntry?.bucket?.dotcomprd}`,
+      //     `--prefix=/${docsetEntry?.prefix?.dotcomprd}`,
+      //     '--deploy',
+      //     `--deployed-url-prefix=s${docsetEntry?.url?.dotcomprd}`,
+      //     '--json',
+      //     '--all-subdirectories',
+      //   ],
+      //   { input: 'y' },
+      // );
+    }
+  } catch (e) {
+    console.log(`Error occurred while running mut-publish: ${e}`);
+  }
+};
