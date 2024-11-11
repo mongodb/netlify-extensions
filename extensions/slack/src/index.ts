065afcd84ff2dd91e40dfbef8123cffde6679f73
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-11-11 18:21:46 -0500

    feat/slack import from extension util

--- extensions/slack/src/index.ts
+++ extensions/slack/src/index.ts
@@ -1,14 +1,17 @@
 // Documentation: https://sdk.netlify.com
 import { NetlifyExtension } from '@netlify/sdk';
+import { Extension } from 'util/extensions';
+
+const ext = new Extension();
 
 const extension = new NetlifyExtension();
 
 extension.addFunctions('./src/functions', {
-	prefix: 'slack',
-	shouldInjectFunction: () => {
-		// If the function is not enabled, return early
-		return !!process.env.SLACK_ENABLED;
-	},
+  prefix: 'slack',
+  shouldInjectFunction: () => {
+    // If the function is not enabled, return early
+    return !!process.env.SLACK_ENABLED;
+  },
 });
 
 export { extension };
