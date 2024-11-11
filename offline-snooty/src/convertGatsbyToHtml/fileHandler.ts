ccba3415e693fb0005d6b09ba6dea06e44c6f94e
Seung Park <seungpark@users.noreply.github.com>
2024-11-11 10:58:28 -0500

    DOP-5128: Scaffolding logic for creating offline docs (#31)
    * initial commit. boilerplate
    
    * startup commits
    
    * make copy of offline snooty and build
    
    * fix syntax
    
    * run format
    
    * biome check --write
    
    * test upload to S3
    
    * remove jsdom for now
    
    * remove jsdom. write comments for todo with 5167
    
    * update build command
    
    * remove jsdom
    
    * add logic to derive filename and bucket from previous extension
    
    * merge conflicts
    
    * run lint
    
    * update package lock

---
+++ offline-snooty/src/convertGatsbyToHtml/fileHandler.ts
@@ -0,0 +1,22 @@
+import { NEW_SNOOTY_PATH } from '..';
+
+const OUTPUT_DIR = 'public';
+const BASE_PATH = `${NEW_SNOOTY_PATH}/snooty/${OUTPUT_DIR}/`;
+
+const getRelativePathToRoot = (filepath: string) => {
+  const filePathParts = filepath.split('/');
+  let relativePath = '';
+  for (let index = filePathParts.length - 2; index >= 0; index--) {
+    const pathPart = filePathParts[index];
+    if (pathPart === OUTPUT_DIR) {
+      break;
+    }
+    relativePath += '../';
+  }
+  return relativePath ? relativePath : './';
+};
+
+export const handleHtmlFile = async (filepath: string) => {
+  // TODO: DOP-5167 use happy-dom to manipulate HTML file.
+  //
+};
--- offline-snooty/src/index.ts
+++ offline-snooty/src/index.ts
@@ -1,14 +1,51 @@
 // Documentation: https://sdk.netlify.com
 import { NetlifyExtension } from '@netlify/sdk';
+import { convertGatsbyToHtml } from './convertGatsbyToHtml';
+import { createSnootyCopy } from './createSnootyCopy';
+import { destroyClient, uploadToS3 } from './uploadToS3';
+import {
+  type BranchEntry,
+  type DocsetsDocument,
+  type ReposBranchesDocument,
+  readEnvConfigs,
+} from './uploadToS3/utils';
 
 const extension = new NetlifyExtension();
+export const NEW_SNOOTY_PATH = `${process.cwd()}/snooty-offline`;
 
-extension.addBuildEventHandler('onPreBuild', () => {
-  // If the build event handler is not enabled, return early
-  if (!process.env.OFFLINE_SNOOTY_ENABLED) {
-    return;
-  }
-  console.log('Hello there.');
-});
+// run this extension after the build and deploy are successful
+extension.addBuildEventHandler(
+  'onSuccess',
+  async ({ netlifyConfig, utils: { run } }) => {
+    // If the build event handler is not enabled, return early
+    if (!process.env.OFFLINE_SNOOTY_ENABLED) {
+      return;
+    }
+    const { bucketName, fileName } = readEnvConfigs({
+      env: netlifyConfig.build.environment.ENV ?? '',
+      docsetEntry:
+        (netlifyConfig.build.environment
+          .DOCSET_ENTRY as unknown as DocsetsDocument) ?? {},
+      repoEntry:
+        (netlifyConfig.build.environment
+          .REPO_ENTRY as unknown as ReposBranchesDocument) ?? {},
+      branchEntry:
+        (netlifyConfig.build.environment
+          .BRANCH_ENTRY as unknown as BranchEntry) ?? {},
+    });
+
+    try {
+      await createSnootyCopy(run, NEW_SNOOTY_PATH);
+      await convertGatsbyToHtml(`${NEW_SNOOTY_PATH}/snooty/public`, fileName);
+      await uploadToS3(`${process.cwd()}/${fileName}`, bucketName, fileName);
+      // TODO: update atlas collection repos_branches to signal offline availability
+    } catch (e) {
+      console.error(e);
+      throw e;
+    } finally {
+      destroyClient();
+    }
+  },
+);
 
 export { extension };
