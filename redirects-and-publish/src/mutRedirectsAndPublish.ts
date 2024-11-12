42f4d9dc7bf712eeb535432bbe5eb8fedef75ab4
biancalaube <bianca.laube@gmail.com>
2024-11-12 14:27:22 -0500

    refactoring

--- redirects-and-publish/src/mutRedirectsAndPublish.ts
+++ redirects-and-publish/src/mutRedirectsAndPublish.ts
@@ -9,12 +9,11 @@ export const mutRedirectsAndPublish = async (
   run: NetlifyPluginUtils['run'],
 ): Promise<void> => {
 
-  console.log(process.cwd());
   // Connect to mongodb and pool.docsets to get bucket
   const docsetEntry = configEnvironment?.DOCSET_ENTRY;
   console.log('Succesfully got docsets entry:', docsetEntry);
   
-  // We want to copy the snooty folder and run `npm run build` instead of `npm run build:no-prefix` as it does in the build.sh --------------------------
+  // We want to copy the snooty folder and run `npm run build` instead of `npm run build:no-prefix` as it does in the build.sh 
   // We do this so when we run mut-publish we are able to uplaod the correct files with the correct paths
   await run.command('rm -f -r running-mut');
   await run.command('mkdir -p running-mut');
@@ -22,19 +21,15 @@ export const mutRedirectsAndPublish = async (
   if (configEnvironment?.SITE_NAME === 'mongodb-snooty') {
     // Since mongodb-snooty is not a content repo the file structure is different and needs to be treated as such
     await run.command('mkdir -p running-mut/snooty');
-    // await run.command(
-    //   'cp -r AWSCLIV2.pkg build.sh bundle.zip CHANGELOG.md code-of-conduct.md component-factory-transformer docs-landing gatsby-browser.js gatsby-config.js gatsby-ssr.js jest.config.js jest-preprocess.js Makefile __mocks__ netlify.toml node_modules package.json package-lock.json plugins public README.md scripts snooty-parser snooty-parser.zip src static stubs tests running-mut/snooty/',
-    // );
     await run.command(
       `rsync -q -i -av --progress  ${process.cwd()} ${process.cwd()}/running-mut/snooty --exclude node_modules --exclude .cache --exclude running-mut`,
     );
+    process.chdir(`${process.cwd()}/running-mut/snooty/repo`);
   } else {
     await run.command('cp -r snooty running-mut');
-    await run.command('ls running-mut/snooty');
+    process.chdir(`${process.cwd()}/running-mut/snooty`);
   }
 
-  await run.command('ls running-mut/snooty');
-  process.chdir(`${process.cwd()}/running-mut/snooty/repo`);
   process.env.GATSBY_MANIFEST_PATH = MANIFEST_PATH;
   // TODO: When uploaded to prod, run this command instead: process.env.PATH_PREFIX = `/${docsetEntry?.prefix?.[configEnvironment.ENV]}`;
   process.env.PATH_PREFIX = `/${docsetEntry?.prefix?.dotcomstg}`;
@@ -88,18 +83,6 @@ export const mutRedirectsAndPublish = async (
     if (!docsetEntry?.bucket || !docsetEntry?.prefix || !docsetEntry?.url) {
       throw new Error();
     }
-
-    console.log(
-      'In the bucket of',
-      docsetEntry?.bucket,
-      docsetEntry?.bucket?.dotcomstg,
-    );
-    console.log(
-      'With a prefix of',
-      docsetEntry?.prefix,
-      `--prefix=${docsetEntry?.prefix?.dotcomstg}`,
-    ); 
-    console.log('And a URL of: ', docsetEntry?.url);
 
     // TODO: In future we change to docsetEntry?.prefix?.[configEnvironment.ENV] and docsetEntry?.url?.[configEnvironment.ENV]
     await run(
