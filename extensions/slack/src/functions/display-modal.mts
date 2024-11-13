91ab899dc93fb256acea8b29c18d06f252cd4fcd
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-11-13 14:09:22 -0500

    feat/slack getDbEnvVars and some refactoring

--- extensions/slack/src/functions/display-modal.mts
+++ extensions/slack/src/functions/display-modal.mts
@@ -2,27 +2,13 @@
 import axios, { type AxiosResponse } from 'axios';
 import { getQSString, validateSlackRequest } from '../process-slack-req.js';
 import { getReposBranchesCollection } from '../dbConnector.js';
-import { getDropDownView, type repoOption } from '../utils/build-modal.js';
+import {
+  displayModal,
+  getDropDownView,
+  type repoOption,
+} from '../utils/build-modal.js';
 import { getDeployableRepos } from '../utils/getRepos.js';
-
-export const displayRepoOptions = async (
-  repos: Array<repoOption>,
-  triggerId: string,
-): Promise<AxiosResponse> => {
-  const repoOptView = getDropDownView(triggerId, repos);
-  //TODO: get slack auth token from dbEnvVars argument in buildEventHandlers
-  const slackToken = process.env.SLACK_AUTH_TOKEN;
-  if (!slackToken) {
-    throw new Error('No Slack token provided');
-  }
-  const slackUrl = 'https://slack.com/api/views.open';
-  return await axios.post(slackUrl, repoOptView, {
-    headers: {
-      Authorization: [`Bearer ${slackToken}`],
-      'Content-type': 'application/json; charset=utf-8',
-    },
-  });
-};
+import { getDbConfig } from 'util/assertDbEnvVars';
 
 export default async (req: Request): Promise<Response> => {
   if (!req.body) {
@@ -31,6 +17,9 @@ export default async (req: Request): Promise<Response> => {
   const requestBody = await new Response(req.body).text();
   const key_val = getQSString(requestBody);
   const trigger_id = key_val.trigger_id;
+
+  const dbEnvVars = getDbConfig();
+  console.log(Object.keys(dbEnvVars));
 
   if (!validateSlackRequest({ requestHeaders: req.headers, requestBody })) {
     console.log('Slack request not validated');
@@ -41,7 +30,7 @@ export default async (req: Request): Promise<Response> => {
 
   const deployableRepos = await getDeployableRepos(reposBranchesCollection);
 
-  const response = await displayRepoOptions(deployableRepos, trigger_id);
+  const response = await displayModal(deployableRepos, trigger_id);
 
   if (!response?.data?.ok) {
     console.log('Response metadata:', response?.data?.response_metadata);
--- extensions/slack/src/utils/build-modal.ts
+++ extensions/slack/src/utils/build-modal.ts
@@ -2,100 +2,25 @@ import type * as mongodb from 'mongodb';
 import type { ReposBranchesDocument } from '../../../search-manifest/src/types.js';
 
 import { capitalizeFirstLetter } from './utils.js';
+import axios, { type AxiosResponse } from 'axios';
 
-export type branchOption = {
-  text: {
-    type: string;
-    text: string;
-  };
-  value: string;
-};
-
-export type repoOption = {
-  label: {
-    type: string;
-    text: string;
-  };
-  options: Array<branchOption>;
-};
-
-type slackBlock = {
-  type: 'plain_text';
-  text: string;
-};
-
-export type dropdownView = {
-  trigger_id: string;
-  view: {
-    type: 'modal';
-    title: slackBlock;
-    submit: slackBlock;
-    close: slackBlock;
-    blocks: [
-      {
-        type: 'input';
-        block_id: 'block_repo_option';
-        label: slackBlock;
-        element: {
-          type: 'multi_static_select';
-          action_id: 'repo_option';
-          placeholder: slackBlock;
-          option_groups: Array<repoOption>;
-        };
-      },
-    ];
-  };
-};
-
-export const buildRepoGroups = async (
-  cursor: mongodb.FindCursor<mongodb.WithId<ReposBranchesDocument>>,
-): Promise<Array<repoOption>> => {
-  const repoOptions: Array<repoOption> = [];
-  for await (const repo of cursor) {
-    const repoName = repo.repoName;
-
-    if (repo?.branches?.length) {
-      const options: Array<branchOption> = [];
-      for (const branch of repo.branches) {
-        const active = branch.active;
-        const branchName = branch.gitBranchName;
-        options.push({
-          text: {
-            type: 'plain_text',
-            text: active ? branchName : `(!inactive) ${branchName}`,
-          },
-          value: `${repoName}/${branchName}`,
-        });
-      }
-      const sortedOptions = sortOptions(options);
-      const repoOption = {
-        label: {
-          type: 'plain_text',
-          text: capitalizeFirstLetter(repoName),
-        },
-        //sort the options by version number
-        options: sortedOptions,
-      };
-      repoOptions.push(repoOption);
-    }
+export const displayModal = async (
+  repos: Array<repoOption>,
+  triggerId: string,
+): Promise<AxiosResponse> => {
+  const repoOptView = getDropDownView(triggerId, repos);
+  //TODO: get slack auth token from dbEnvVars argument in buildEventHandlers
+  const slackToken = process.env.SLACK_AUTH_TOKEN;
+  if (!slackToken) {
+    throw new Error('No Slack token provided');
   }
-  return repoOptions.sort((repoOne, repoTwo) =>
-    repoOne.label.text.localeCompare(repoTwo.label.text),
-  );
-};
-
-export const sortOptions = (options: Array<branchOption>) => {
-  const sortedOptions = options.sort((branchOne, branchTwo) =>
-    branchTwo.text.text
-      .toString()
-      .replace(/\d+/g, (n) => (+n + 100000).toString())
-      .localeCompare(
-        branchOne.text.text
-          .toString()
-          .replace(/\d+/g, (n) => (+n + 100000).toString()),
-      ),
-  );
-  return sortedOptions;
+  const slackUrl = 'https://slack.com/api/views.open';
+  return await axios.post(slackUrl, repoOptView, {
+    headers: {
+      Authorization: [`Bearer ${slackToken}`],
+      'Content-type': 'application/json; charset=utf-8',
+    },
+  });
 };
 
 export function getDropDownView(
@@ -140,3 +65,47 @@ export function getDropDownView(
     },
   };
 }
+
+export type branchOption = {
+  text: {
+    type: string;
+    text: string;
+  };
+  value: string;
+};
+
+export type repoOption = {
+  label: {
+    type: string;
+    text: string;
+  };
+  options: Array<branchOption>;
+};
+
+type slackBlock = {
+  type: 'plain_text';
+  text: string;
+};
+
+export type dropdownView = {
+  trigger_id: string;
+  view: {
+    type: 'modal';
+    title: slackBlock;
+    submit: slackBlock;
+    close: slackBlock;
+    blocks: [
+      {
+        type: 'input';
+        block_id: 'block_repo_option';
+        label: slackBlock;
+        element: {
+          type: 'multi_static_select';
+          action_id: 'repo_option';
+          placeholder: slackBlock;
+          option_groups: Array<repoOption>;
+        };
+      },
+    ];
+  };
+};
