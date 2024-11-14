e4c3227d269e7acc04782c4053271d5d9c642423
Seung Park <seung.park@mongodb.com>
2024-11-14 13:43:12 -0500

    remove recursive log

--- extensions/offline-snooty/src/convertGatsbyToHtml/index.ts
+++ extensions/offline-snooty/src/convertGatsbyToHtml/index.ts
@@ -5,16 +5,22 @@
  * @param path full directory path of gatsby output
  */
 
-import { existsSync, promises as fsPromises } from 'node:fs';
-import { join } from 'node:path';
-import { create } from 'tar';
-import { handleHtmlFile } from './fileHandler';
-import { IMAGE_EXT } from './imageExtensions';
+import { existsSync, promises as fsPromises } from "node:fs";
+import { join } from "node:path";
+import { create } from "tar";
+import { handleHtmlFile } from "./fileHandler";
+import { IMAGE_EXT } from "./imageExtensions";
 
 type fileUpdateLog = {
   processedHtmlFiles: string[];
   removedFiles: string[];
   filePathsPerDir: { [key: string]: string[] };
+};
+
+const log: fileUpdateLog = {
+  processedHtmlFiles: [],
+  removedFiles: [],
+  filePathsPerDir: {},
 };
 
 // get all full directory pathnames leading up to current path
@@ -24,12 +30,12 @@ function getParentPaths(directoryPath: string): string[] {
   let currentDirectory = directoryPath;
   while (!isRoot) {
     res.push(currentDirectory);
-    const currentParts = currentDirectory.split('/');
-    currentDirectory = currentParts.slice(0, -1).join('/');
+    const currentParts = currentDirectory.split("/");
+    currentDirectory = currentParts.slice(0, -1).join("/");
     // note: can update this to be read from original rootDirectoryPath of scanFileTree.
     isRoot =
-      currentParts[currentParts.length - 1] === 'public' &&
-      currentParts[currentParts.length - 2] === 'snooty';
+      currentParts[currentParts.length - 1] === "public" &&
+      currentParts[currentParts.length - 2] === "snooty";
   }
 
   return res;
@@ -37,21 +43,13 @@ function getParentPaths(directoryPath: string): string[] {
 
 // traverses into a directory, and handles each file.
 // each file type handler should handle what to do with current file type
-async function scanFileTree(
-  directoryPath: string,
-  pathToRoot: string,
-  fileUpdateLog: fileUpdateLog = {
-    processedHtmlFiles: [],
-    removedFiles: [],
-    filePathsPerDir: {},
-  },
-) {
+async function scanFileTree(directoryPath: string, pathToRoot: string) {
   if (!existsSync(directoryPath)) {
     console.error(`no directory at ${directoryPath}`);
-    return fileUpdateLog;
+    return;
   }
-  if (!fileUpdateLog.filePathsPerDir[directoryPath]) {
-    fileUpdateLog.filePathsPerDir[directoryPath] = [];
+  if (!log.filePathsPerDir[directoryPath]) {
+    log.filePathsPerDir[directoryPath] = [];
   }
 
   const files = await fsPromises.readdir(directoryPath);
@@ -59,39 +57,38 @@ async function scanFileTree(
     const filename = join(directoryPath, file);
     const stat = await fsPromises.stat(filename);
 
-    const extName = filename.split('.').pop() ?? '';
+    const extName = filename.split(".").pop() ?? "";
     console.log(
-      `extName ${extName} of filename ${filename} isDIrectory ${stat.isDirectory()}`,
+      `extName ${extName} of filename ${filename} isDIrectory ${stat.isDirectory()}`
     );
 
     if (stat.isDirectory()) {
-      scanFileTree(filename, '../' + pathToRoot, fileUpdateLog); //recurse
-    } else if (extName.endsWith('html')) {
-      await handleHtmlFile(filename, pathToRoot || './');
+      scanFileTree(filename, "../" + pathToRoot); //recurse
+    } else if (extName.endsWith("html")) {
+      await handleHtmlFile(filename, pathToRoot || "./");
       const allParentPaths = getParentPaths(directoryPath);
       for (const parentPath of allParentPaths) {
-        fileUpdateLog.filePathsPerDir[parentPath].push(filename);
+        log.filePathsPerDir[parentPath].push(filename);
       }
     } else if (IMAGE_EXT.has(extName)) {
       continue;
     } else {
       // delete the file
       await fsPromises.rm(filename);
-      console.log('skipping removing file ', filename);
+      console.log("skipping removing file ", filename);
 
-      fileUpdateLog.removedFiles.push(filename);
+      log.removedFiles.push(filename);
     }
   }
-  return fileUpdateLog;
 }
 
 export const convertGatsbyToHtml = async (
   gatsbyOutputPath: string,
-  fileName: string,
+  fileName: string
 ): Promise<void> => {
-  const log = await scanFileTree(gatsbyOutputPath, '');
-  console.log('>>>>>>>>>> converted gatsby results <<<<<<<<<<<<<');
-  console.log(JSON.stringify(log));
+  await scanFileTree(gatsbyOutputPath, "");
+  console.log(">>>>>>>>>> converted gatsby results <<<<<<<<<<<<<");
+  // console.log(JSON.stringify(log));
 
   // remove empty directories
   for (const [path, filenames] of Object.entries(log.filePathsPerDir)) {
@@ -106,6 +103,6 @@ export const convertGatsbyToHtml = async (
       file: fileName,
       cwd: gatsbyOutputPath,
     },
-    ['./'],
+    ["./"]
   );
 };
