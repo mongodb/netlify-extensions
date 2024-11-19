25cf534282c85204c2cd6bb460c646d0fb4ffec8
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-11-19 15:07:34 -0500

    DOP-5174 add comments

--- extensions/populate-metadata/src/updateConfig.ts
+++ extensions/populate-metadata/src/updateConfig.ts
@@ -79,7 +79,7 @@ export const updateConfig = async ({
   dbEnvVars: StaticEnvVars;
 }): Promise<void> => {
   // Checks if build was triggered by a webhook
-  // TODO: add more specific logic dependent on hook title, url, body, etc. once Slack deploy apps have been implemented
+  // TODO: DOP-5201, add more specific logic dependent on hook title, url, body, etc. once Slack deploy apps have been implemented
   const isBuildHookDeploy = !!(
     configEnvironment.INCOMING_HOOK_URL && configEnvironment.INCOMING_HOOK_TITLE
   );
@@ -117,10 +117,15 @@ export const updateConfig = async ({
       process.env.REPO_NAME ??
       (process.env.REPOSITORY_URL?.split('/')?.pop() as string);
   } else {
-    // Branch name and repo name to deploy sent as values in Build Hook payload if in dotcomprd or dotcomstg environments
-    [branchName, repoName] = configEnvironment?.INCOMING_HOOK_BODY?.split(
-      '',
-    ) as string[];
+    branchName =
+      process.env.BRANCH_NAME ?? (configEnvironment.BRANCH as string);
+    repoName =
+      process.env.REPO_NAME ??
+      (process.env.REPOSITORY_URL?.split('/')?.pop() as string);
+    // TODO: DOP-5201, Branch name and repo name to deploy sent as values in Build Hook payload if in dotcomprd or dotcomstg environments
+    // [branchName, repoName] = configEnvironment?.INCOMING_HOOK_BODY?.split(
+    //   '',
+    // ) as string[];
   }
 
   if (!branchName || !repoName) {
