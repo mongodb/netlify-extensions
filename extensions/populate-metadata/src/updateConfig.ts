b578280c07144abc1784bc6ebc857c5537de98b3
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-12-03 12:01:03 -0500

    DOP-5215 use github pwd w silent option

--- extensions/populate-metadata/src/updateConfig.ts
+++ extensions/populate-metadata/src/updateConfig.ts
@@ -162,7 +162,7 @@ export const updateConfig = async ({
 
     const botPwd = process.env.GITHUB_BOT_PWD;
     await run.command(
-      `git clone -b ${branchName} https://github.com/${orgName}/${repoName}.git `,
+      `git clone -b ${branchName} https://${botPwd}@github.com/${orgName}/${repoName}.git -s`,
     );
   }
   // Set process.env SNOOTY_ENV and PREFIX_PATH environment variables for frontend to retrieve at build time
