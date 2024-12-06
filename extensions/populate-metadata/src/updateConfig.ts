69dcc3b4d56768a7df611792414ce19edb6f19bc
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-12-06 13:10:04 -0800

    DOP-5215 remove git config after cloning

--- extensions/populate-metadata/src/updateConfig.ts
+++ extensions/populate-metadata/src/updateConfig.ts
@@ -156,31 +156,29 @@ export const updateConfig = async ({
       `echo "Cloning content repo \n repo ${repoName}, branchName: ${branchName}, orgName: ${orgName}" `,
     );
 
-    // const octokit = new Octokit({
-    //   auth: process.env.GITHUB_BOT_PWD,
-    // });
-
-    // const response = await octokit.request(
-    //   'GET /repos/{owner}/{repo}/branches/{branch}',
-    //   {
-    //     owner: orgName,
-    //     repo: repoName,
-    //     branch: branchName,
-    //   },
-    // );
-    // console.log(response);
-
-    // cosnt responseTwo = await octokit.actions.downloadArtifact({})
-    // await run.command(
-    //   `if [ -d '${repoName}' ]; then \n echo 'bi connector dir exists' \n fi`,
-    // );
-
     if (fs.existsSync(`${process.cwd()}/${repoName}`)) {
+      // const octokit = new Octokit({
+      //   auth: process.env.GITHUB_BOT_PWD,
+      // });
+
+      // const response = await octokit.request(
+      //   'GET /repos/{owner}/{repo}/branches/{branch}',
+      //   {
+      //     owner: orgName,
+      //     repo: repoName,
+      //     branch: branchName,
+      //   },
+      // );
+      // console.log(response);
+
+      // cosnt responseTwo = await octokit.actions.downloadArtifact({})
+      // await run.command(
+      //   `if [ -d '${repoName}' ]; then \n echo 'bi connector dir exists' \n fi`,
+      // );
+
       await run.command(`rm -r ${repoName}`);
     }
 
-    const botPwd = process.env.GITHUB_BOT_PWD;
-    // process.env.GIT_PASSWORD = botPwd;
     // const askPassFilePath = `${process.cwd()}/.ssh-askpass`;
 
     // // await run.command(`touch testing.txt`);
@@ -197,6 +195,10 @@ export const updateConfig = async ({
     await run.command(
       `git clone -b ${branchName} https://docs-builder-bot:${process.env.GITHUB_BOT_PWD}@github.com/${orgName}/${repoName}.git -s`,
     );
+
+    if (fs.existsSync(`${repoName}.git/config`)) {
+      await run.command(`rm -r ${repoName}.git/config`);
+    }
   }
   // Set process.env SNOOTY_ENV and PREFIX_PATH environment variables for frontend to retrieve at build time
   process.env.SNOOTY_ENV = buildEnvironment;
