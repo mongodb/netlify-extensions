895eb5bd59d45795928db9a5fcd8a611549e334d
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-12-06 09:58:07 -0800

    DOP-5215 comment out some logs

--- extensions/populate-metadata/src/updateConfig.ts
+++ extensions/populate-metadata/src/updateConfig.ts
@@ -185,8 +185,8 @@ export const updateConfig = async ({
     await run.command(`echo 'hello' > ${askPassFilePath}`);
     await run.command('ls');
 
-    const data = fs.readFileSync(askPassFilePath, 'utf-8');
-    console.log(data);
+    // const data = fs.readFileSync(askPassFilePath, 'utf-8');
+    // console.log(data);
 
     process.env.SSH_ASKPASS = `${askPassFilePath}`;
 
