b04c05bbdf7162fe9b8c16bf8c48f7ec1a46d866
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-12-10 09:58:42 -0800

    DOP-5215 lint and format

--- libs/util/src/databaseConnection/fetchDocumentsData.ts
+++ libs/util/src/databaseConnection/fetchDocumentsData.ts
@@ -2,7 +2,6 @@ import type * as mongodb from 'mongodb';
 import { getClusterZeroDb } from './clusterZeroConnector';
 import type { DocumentsDocument } from './types';
 
-
 // Get Documents collection of Snooty_<xyz> database
 export const getDocumentsCollection = async ({
   clusterZeroURI,
@@ -15,7 +14,6 @@ export const getDocumentsCollection = async ({
   collectionName: string;
   extensionName?: string;
 }): Promise<mongodb.Collection<DocumentsDocument>> => {
-
   const dbSession = await getClusterZeroDb({
     clusterZeroURI,
     databaseName,
