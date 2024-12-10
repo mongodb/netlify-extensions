a58f0609dfd0a833aaea356a73cc322a2d237480
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-12-09 17:42:29 -0800

    DOP-5168 cleaning redirects and publish

--- extensions/redirects-and-publish/src/mutRedirectsAndPublish.ts
+++ extensions/redirects-and-publish/src/mutRedirectsAndPublish.ts
@@ -1,48 +1,63 @@
 import type { ConfigEnvironmentVariables } from 'util/extension';
 import type { NetlifyPluginUtils } from '@netlify/build';
-const MUT_VERSION = process.env.MUT_VERSION;
-const MANIFEST_PATH = `${process.cwd()}/bundle.zip`;
+import type { BranchEntry } from 'util/databaseConnection/types';
+
+const getRepoAliases = (branchEntry: BranchEntry) => {
+  // use slice() to create shallow copy of the branch's aliases
+  const urlAliases = branchEntry.urlAliases
+    ? branchEntry.urlAliases.slice()
+    : [];
+
+  if (
+    !!branchEntry.publishOriginalBranchName &&
+    !urlAliases.includes(branchEntry.gitBranchName)
+  ) {
+    // Add current branch to list of aliases
+    urlAliases.push(branchEntry.gitBranchName);
+  }
+  if (urlAliases.length === 0) {
+    urlAliases.push(branchEntry.urlSlug);
+  }
+  return urlAliases;
+};
+
+const setMutEnvVars = () => {
+  process.env.GATSBY_MANIFEST_PATH = `${process.cwd()}/bundle.zip`;
+  process.env.GATSBY_PARSER_USER = 'buildbot';
+  //TODO: Mut and populate-metadata extensions use different env variable names for the same values (set to team wide in future)
+  process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_S3_SECRET_ACCESS_KEY;
+  process.env.AWS_ACCESS_KEY_ID = process.env.AWS_S3_ACCESS_KEY_ID;
+  if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
+    throw new Error('Credentials not found');
+  }
+};
+
 export const mutRedirectsAndPublish = async (
   configEnvironment: ConfigEnvironmentVariables,
   run: NetlifyPluginUtils['run'],
 ): Promise<void> => {
-  // Get data from pool.docsets to get bucket
+  const repoName = configEnvironment.REPO_ENTRY?.repoName;
+
+  // Get docset entry and branch entry from NetlifyConfig
   const docsetEntry = configEnvironment?.DOCSET_ENTRY;
   const branchEntry = configEnvironment?.BRANCH_ENTRY;
   if (!docsetEntry) {
     throw new Error('Unable to retrive DOCSET_ENTRY');
   }
   if (!branchEntry) {
-    throw new Error('Unable to retrive BRANCH_ENTRY');
-  }
-  console.log('Succesfully got docsets_entry:', docsetEntry);
-  console.log('Succesfully got branch_entry', branchEntry);
-
-  // Get the array of the all the possible alisas
-  // using slice() to create shallow copy so they're no errors from netlify about trying to write a read-only variable
-  const urlAliases = branchEntry.urlAliases
-    ? branchEntry.urlAliases.slice()
-    : [];
-
-  // Add current branch  to list of aliases
-  if (
-    !!branchEntry.publishOriginalBranchName &&
-    !urlAliases.includes(branchEntry.gitBranchName)
-  ) {
-    urlAliases.push(branchEntry.gitBranchName);
-  }
-  if (urlAliases.length === 0) {
-    urlAliases.push(branchEntry.urlSlug);
+    throw new Error('Unable to retrieve BRANCH_ENTRY');
   }
 
-  // These aliases are used to publish page info into the correct subdirectories in the bucket
-  console.log('The urlAliases are', urlAliases);
+  // Get the array of the all the possible aliases, used to publish page info into the correct subdirectories in the bucket
+  const urlAliases = getRepoAliases(branchEntry);
 
-  // We want to copy the snooty folder and run `npm run build` instead of `npm run build:no-prefix` as it does in the build.sh
-  // We do this so when we run mut-publish we are able to uplaod the correct files with the correct paths
+  // Set environment variables for Mut
+  setMutEnvVars();
+
+  // Copy the Snooty frontend folder and rerun the frontend build WITH a Gatsby prefix (instead of `npm run build:no-prefix` as done in the build.sh)
   await run.command('rm -f -r running-mut');
   await run.command('mkdir -p running-mut');
-  // TODO: this should also happen for dotcomprd if configEnvironment.ENV == dotcomstg or (dotcomprd)
+  // TODO: this should also happen for dotcomprd if configEnvironment.ENV == dotcomprd
   if (configEnvironment?.ENV === 'dotcomstg') {
     await run.command('mkdir -p running-mut/snooty');
     await run.command(
@@ -51,41 +66,34 @@ export const mutRedirectsAndPublish = async (
     process.chdir(`${process.cwd()}/running-mut/snooty/repo`);
   }
 
-  console.log('Downloading Mut...', configEnvironment?.SITE_NAME);
+  console.log(`Downloading and unzipping mut for ${repoName}`);
   await run('curl', [
     '-L',
     '-o',
     'mut.zip',
-    `https://github.com/mongodb/mut/releases/download/v${MUT_VERSION}/mut-v${MUT_VERSION}-linux_x86_64.zip`,
+    `https://github.com/mongodb/mut/releases/download/v${process.env.MUT_VERSION}/mut-v${process.env.MUT_VERSION}-linux_x86_64.zip`,
   ]);
   await run.command('unzip -d . -qq mut.zip');
 
-  process.env.GATSBY_MANIFEST_PATH = MANIFEST_PATH;
-  process.env.GATSBY_PARSER_USER = 'buildbot';
-  //TODO: Mut and populate-metadata extensions use different env variable names for the same values (set to team wide in future)
-  process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_S3_SECRET_ACCESS_KEY;
-  process.env.AWS_ACCESS_KEY_ID = process.env.AWS_S3_ACCESS_KEY_ID;
-  if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-    throw new Error('Credentials not found');
-  }
-
   for (const alias of urlAliases) {
-    // Building snooty ------------------------------------------------------------
+    // Building frontend ------------------------------------------------------------
     // TODO: When uploaded to prod, run this command instead: process.env.PATH_PREFIX = `/${docsetEntry?.prefix?.[configEnvironment.ENV]}`; (DOP-5178)
     const prefix = alias
       ? `/${docsetEntry.prefix.dotcomstg}/${alias}`
       : `/${docsetEntry.prefix.dotcomstg}`;
     process.env.PATH_PREFIX = prefix;
+
     await run.command('npm ci');
     await run.command('npm run clean');
     await run.command('npm run build');
 
     // Running mut-redirects -------------------------------------------------------
     try {
-      console.log('Running mut-redirects...');
-      console.log('The repo name is', configEnvironment.REPO_ENTRY?.repoName);
+      console.log(
+        `Running mut-redirects... for ${repoName} with alias ${alias}`,
+      );
       await run.command(
-        `${process.cwd()}/mut/mut-redirects ${configEnvironment.REPO_ENTRY?.repoName}/config/redirects -o public/.htaccess`,
+        `${process.cwd()}/mut/mut-redirects ${repoName}/config/redirects -o public/.htaccess`,
       );
     } catch (e) {
       console.log(`Error occurred while running mut-redirects: ${e}`);
