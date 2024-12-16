bb452b531d818ed26ee7084947e2c3e14dad30a8
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-12-16 10:27:07 -0500

    DOP-5238 add conditional for cloning docs-laravel repo

--- extensions/populate-metadata/src/updateConfig.ts
+++ extensions/populate-metadata/src/updateConfig.ts
@@ -92,6 +92,11 @@ const cloneContentRepo = async ({
     `git clone -b ${branchName} https://${process.env.GITHUB_BOT_USERNAME}:${process.env.GITHUB_BOT_PWD}@github.com/${orgName}/${repoName}.git -s`,
   );
 
+  if (repoName === 'DOCS_LARAVEL') {
+    await run.command('git submodule update --init --recursive');
+    await run.command('mkdir source && cp -r laravel-mongodb/docs/* source');
+  }
+
   // Remove git config as it stores the connection string in plain text
   if (fs.existsSync(`${repoName}/.git/config`)) {
     await run.command(`rm -r ${repoName}/.git/config`);
