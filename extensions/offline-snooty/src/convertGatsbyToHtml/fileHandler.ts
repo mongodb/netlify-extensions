e6df22479752fe7d9bce713851696f291a68793c
Seung Park <seungpark@users.noreply.github.com>
2024-12-17 16:19:38 -0500

    save images to directory and update img src (#53)

--- extensions/offline-snooty/src/convertGatsbyToHtml/fileHandler.ts
+++ extensions/offline-snooty/src/convertGatsbyToHtml/fileHandler.ts
@@ -4,7 +4,10 @@ import {
   type Node,
   Window,
 } from 'happy-dom';
-import { promises as fsPromises } from 'node:fs';
+import { promises as fsPromises, createWriteStream } from 'node:fs';
+import { Readable, promises } from 'node:stream';
+import { join } from 'node:path';
+import { PUBLIC_OUTPUT_PATH } from '..';
 
 function updateToRelativePaths(nodeList: Node[], prefix: string) {
   // for links: href = relativePath + href + index.html
@@ -18,9 +21,46 @@ function updateToRelativePaths(nodeList: Node[], prefix: string) {
       );
       node.setAttribute('href', targetHref);
     } else if (node instanceof HTMLImageElement && node.src.startsWith('/')) {
-      node.src = (prefix + node.src).replace(/\/+/, '/');
+      node.src = (prefix + node.src).replace(/\/+/g, '/');
     }
   }
+}
+
+async function saveImageAndUpdateSrc(img: HTMLImageElement, prefix: string) {
+  const url = new URL(img.getAttribute('src'));
+  // get the path to save the file to
+  const targetFileName = url.pathname;
+  const fileNameParts = targetFileName.split('/');
+
+  // create the directory, at same URL path as original image src
+  const targetDir = `${PUBLIC_OUTPUT_PATH}/images/${fileNameParts.slice(0, fileNameParts.length - 1).join('/')}`;
+  const targetFilePath = `${targetDir}/${fileNameParts[fileNameParts.length - 1]}`;
+  await fsPromises.mkdir(targetDir, { recursive: true });
+
+  // save res to file
+  const res = await fetch(url);
+  const filestream = createWriteStream(targetFilePath);
+  await promises.finished(
+    Readable.fromWeb(res.body as ReadableStream).pipe(filestream),
+  );
+
+  // update img src to be relative to above
+  img.src = join(prefix, 'images', targetFileName);
+}
+
+async function downloadRemoteImages(
+  document: Window['document'],
+  prefix: string,
+) {
+  // find all remote img src
+  const imgNodes = document.querySelectorAll('img[src^=http]');
+  const promises = [];
+
+  for (const imgNode of imgNodes.values()) {
+    promises.push(saveImageAndUpdateSrc(imgNode as HTMLImageElement, prefix));
+  }
+
+  return Promise.all(promises);
 }
 
 function removeScripts(document: Window['document']) {
@@ -47,7 +87,7 @@ export const handleHtmlFile = async (
 
   const links = document.querySelectorAll('a');
   const images = document.querySelectorAll('img');
-  // TODO as part of DOP-5196
+  await downloadRemoteImages(document, relativePath);
   updateToRelativePaths([...links, ...images], relativePath ?? './');
   removeScripts(document);
 
